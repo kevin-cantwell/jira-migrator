72cc61130886ec352a69c4f1ab0f853219ff3014
Kevin Cantwell <kevin.cantwell@gmail.com>
2022-02-25 17:04:15 -0500

    rebuild with urfave cli

--- cmd/jira-migrator/main.go
+++ cmd/jira-migrator/main.go
@@ -1,214 +1,317 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 
-	jira "github.com/andygrunwald/go-jira"
 	"github.com/pkg/errors"
-	yaml "gopkg.in/yaml.v2"
-)
 
-var (
-	configFile string
+	jira "github.com/andygrunwald/go-jira"
+	cli "github.com/urfave/cli/v2"
+	yaml "gopkg.in/yaml.v2"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	flag.StringVar(&configFile, "config", "config.yaml", "The configuration file to use.")
-	flag.StringVar(&configFile, "c", "config.yaml", "The configuration file to use.")
-	flag.Usage = func() {
-		fmt.Println(`Usage:
-    jira-migrator OPTIONS COMMAND
-Options:
-    --config,-c                 The configuration file to use. (default "config.json")
-Commands:
-    migrate ISSUE_KEY           Migrate issue from server to cloud. Also migrates child issues.
-    server users PROJECT_KEY    List server users. Helpful for setting user_mappings configs.
-    cloud users PROJECT_KEY     List cloud user metadata. Helpful for setting the user_mappings config.
-`)
-	}
-	flag.Parse()
+	cliApp := &cli.App{
+		Name:  "jira-migrator",
+		Usage: "migrate tickets from one server to another",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "config",
+				Aliases: []string{"c"},
+				Value:   "config.yaml",
+				Usage:   "The configuration file to use.",
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "migrate",
+				Usage: "Migrate issues from one server to another",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "jql",
+						Usage:    "The JQL query string to execute against the configured \"from\" server.",
+						Required: true,
+					},
+				},
+				Action: func(c *cli.Context) error {
+					c.String("config")
+					app, err := NewApp(c.String("config"))
+					if err != nil {
+						return err
+					}
 
-	configFile, err := os.Open(configFile)
+					issues, err := app.QueryIssues(c.String("jql"))
+					if err != nil {
+						return err
+					}
+
+					// migrate issues in the order they were returned from the query
+					for _, issue := range issues {
+						fmt.Println(issue.Key)
+						if _, err := app.MigrateIssue(&issue); err != nil {
+							return err
+						}
+					}
+
+					return nil
+				},
+			},
+		},
+	}
+
+	err := cliApp.Run(os.Args)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	var config Config
-	if err := yaml.NewDecoder(configFile).Decode(&config); err != nil {
-		panic(err)
-	}
-
-	serverAuth := jira.BasicAuthTransport{
-		Username: config.Server.Username,
-		Password: config.Server.Password,
-	}
-	server, err := jira.NewClient(serverAuth.Client(), "https://"+config.Server.Host)
-	if err != nil {
-		panic(err)
-	}
-
-	cloudAuth := jira.BasicAuthTransport{
-		Username: config.Cloud.Email,
-		Password: config.Cloud.ApiKey,
-	}
-	cloud, err := jira.NewClient(cloudAuth.Client(), "https://"+config.Cloud.Host)
-	if err != nil {
-		panic(err)
-	}
-
-	cloudUsers, resp, err := getCloudUsers(cloud, config.Cloud.ProjectKey)
-	if err != nil {
-		respErrExit(resp, err)
-	}
-
-	userLookup := map[string]jira.User{}
-	for _, user := range cloudUsers {
-		userLookup[user.EmailAddress] = user
-	}
-
-	app := App{
-		Server:     server,
-		Cloud:      cloud,
-		Config:     config,
-		UserLookup: userLookup,
-	}
-
-	switch cmd := flag.Arg(0); cmd {
-	case "migrate":
-		issueKey := flag.Arg(1)
-		if resp, err := app.MigrateIssue("", issueKey); err != nil {
-			respErrExit(resp, err)
-		}
-	default:
-		log.Printf("Invalid command: %q\n", cmd)
-		os.Exit(1)
-	}
+	return
 }
 
 func respErrExit(resp *jira.Response, err error) {
-	if resp != nil {
-		io.Copy(log.Writer(), resp.Body)
-		log.Print("\n")
-	}
+	dumpResponse(resp)
 	panic(err)
 }
 
+func dumpResponse(resp *jira.Response) {
+	if resp != nil {
+		io.Copy(os.Stderr, resp.Body)
+		fmt.Fprint(os.Stderr, "\n")
+	}
+}
+
 type Config struct {
-	Server struct {
+	From struct {
 		Host       string `yaml:"host"`
 		Username   string `yaml:"username"`
 		Password   string `yaml:"password"`
 		ProjectKey string `yaml:"project_key"`
-	} `yaml:"server"`
-	Cloud struct {
+	} `yaml:"from"`
+	To struct {
 		Host       string `yaml:"host"`
-		Email      string `yaml:"email"`
-		ApiKey     string `yaml:"api_key"`
+		Username   string `yaml:"username"`
+		Password   string `yaml:"password"`
 		ProjectKey string `yaml:"project_key"`
-	} `yaml:"cloud"`
-	UserMappings   map[string]string `yaml:"user_mappings"`
-	StatusMappings map[string]string `yaml:"status_mappings"`
+	} `yaml:"to"`
+	// UserMappings   map[string]string `yaml:"user_mappings"`
+	// StatusMappings map[string]string `yaml:"status_mappings"`
 }
 
 type App struct {
-	Server     *jira.Client
-	Cloud      *jira.Client
+	From       *jira.Client
+	To         *jira.Client
 	Config     Config
 	UserLookup map[string]jira.User
 }
 
-func (app *App) MigrateIssue(parentKey, issueKey string) (*jira.Response, error) {
-	oldIssue, resp, err := app.Server.Issue.Get(issueKey, &jira.GetQueryOptions{
-		FieldsByKeys: true,
-	})
+func NewApp(pathToConfig string) (*App, error) {
+	configFile, err := os.Open(pathToConfig)
 	if err != nil {
-		return resp, errors.Wrapf(err, "Error fetching issue %s", issueKey)
+		return nil, err
 	}
 
+	var config Config
+	if err := yaml.NewDecoder(configFile).Decode(&config); err != nil {
+		return nil, err
+	}
+
+	from, err := jira.NewClient((&jira.BasicAuthTransport{
+		Username: config.From.Username,
+		Password: config.From.Password,
+	}).Client(), "https://"+config.From.Host)
+	if err != nil {
+		return nil, err
+	}
+
+	to, err := jira.NewClient((&jira.BasicAuthTransport{
+		Username: config.To.Username,
+		Password: config.To.Password,
+	}).Client(), "https://"+config.To.Host)
+	if err != nil {
+		return nil, err
+	}
+
+	toUsers, err := getUsers(to, config.To.ProjectKey)
+	if err != nil {
+		return nil, err
+	}
+
+	userLookup := map[string]jira.User{}
+	for _, user := range toUsers {
+		userLookup[user.EmailAddress] = user
+	}
+
+	return &App{
+		From:       from,
+		To:         to,
+		Config:     config,
+		UserLookup: userLookup,
+	}, nil
+}
+
+func (app *App) QueryIssues(jql string) ([]jira.Issue, error) {
+	var issues []jira.Issue
+	if err := app.From.Issue.SearchPages(jql, &jira.SearchOptions{
+		Expand: "names",
+	}, func(issue jira.Issue) error {
+		issues = append(issues, issue)
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return issues, nil
+}
+
+func (app *App) MigrateIssue(issue *jira.Issue) (*jira.Issue, error) {
 	newIssue := jira.Issue{
 		Fields: &jira.IssueFields{
 			Project: jira.Project{
-				Key: app.Config.Cloud.ProjectKey,
+				Key: app.Config.To.ProjectKey,
 			},
 			Type: jira.IssueType{
-				Name: oldIssue.Fields.Type.Name,
+				Name: issue.Fields.Type.Name,
 			},
-			Summary:     oldIssue.Fields.Summary,
-			Description: oldIssue.Fields.Description,
+			Summary:     issue.Fields.Summary,
+			Description: issue.Fields.Description,
 		},
 	}
 
-	if parentKey != "" {
-		newIssue.Fields.Parent = &jira.Parent{
-			Key: parentKey,
-		}
-	}
-
-	if reporter := oldIssue.Fields.Reporter; reporter != nil {
+	if reporter := issue.Fields.Reporter; reporter != nil {
 		cloudUser, ok := app.UserLookup[reporter.EmailAddress]
 		if ok {
 			newIssue.Fields.Reporter = &jira.User{AccountID: cloudUser.AccountID}
 		}
 	}
 
-	if assignee := oldIssue.Fields.Assignee; assignee != nil {
+	if assignee := issue.Fields.Assignee; assignee != nil {
 		cloudUser, ok := app.UserLookup[assignee.EmailAddress]
 		if ok {
 			newIssue.Fields.Assignee = &jira.User{AccountID: cloudUser.AccountID}
 		}
 	}
 
-	createdIssue, resp, err := app.Cloud.Issue.Create(&newIssue)
+	createdIssue, resp, err := app.To.Issue.Create(&newIssue)
 	if err != nil {
-		return resp, errors.Wrapf(err, "Error creating issue from %s", issueKey)
+		dumpResponse(resp)
+		return nil, errors.Wrap(err, "Error creating issue")
+	}
+
+	if _, resp, err := app.To.Issue.AddRemoteLink(createdIssue.ID, &jira.RemoteLink{
+		Application: &jira.RemoteLinkApplication{
+			Type: "jira.etsycorp.com",
+			Name: "Migrated Issue",
+		},
+		Object: &jira.RemoteLinkObject{
+			URL:     "https://jira.etsycorp.com/browse/" + issue.Key,
+			Title:   issue.Key,
+			Summary: issue.Fields.Summary,
+		},
+	}); err != nil {
+		dumpResponse(resp)
+		return nil, errors.Wrap(err, "Error creating remote link")
 	}
 
 	// Comments can't be set on create. They must be added later
-	for _, comment := range oldIssue.Fields.Comments.Comments {
-		if _, resp, err = app.Cloud.Issue.AddComment(createdIssue.ID, &jira.Comment{
-			Name: comment.Name,
-			// It's impossible to set a different author than "self",
-			// so just indicate who wrote this originally in the body of the comment.
-			Body:       comment.Author.EmailAddress + " wrote:\n\n" + comment.Body,
-			Visibility: comment.Visibility,
-		}); err != nil {
-			return resp, errors.Wrapf(err, "Error adding comment to %s", createdIssue.Key)
-		}
-	}
-
-	transitions, resp, err := app.Cloud.Issue.GetTransitions(createdIssue.ID)
-	if err != nil {
-		return resp, errors.Wrapf(err, "Error fetching transitions for %s", createdIssue.Key)
-	}
-	for _, transition := range transitions {
-		if transition.To.Name == oldIssue.Fields.Status.Name {
-			if resp, err := app.Cloud.Issue.DoTransition(createdIssue.ID, transition.ID); err != nil {
-				return resp, errors.Wrapf(err, "Error transitioning issue %s to %q", createdIssue.Key, transition.To.Name)
+	if issue.Fields.Comments != nil {
+		for _, comment := range issue.Fields.Comments.Comments {
+			if _, resp, err := app.To.Issue.AddComment(createdIssue.ID, &jira.Comment{
+				Name: comment.Name,
+				// It's impossible to set a different author than "self",
+				// so just indicate who wrote this originally in the body of the comment.
+				Body:       comment.Author.EmailAddress + " wrote:\n\n" + comment.Body,
+				Visibility: comment.Visibility,
+			}); err != nil {
+				dumpResponse(resp)
+				return nil, errors.Wrapf(err, "Error adding comment to %s", createdIssue.Key)
 			}
 		}
 	}
 
-	fmt.Println("Migrated:", oldIssue.Key)
-
-	// TODO figure out how to discover epic children
-	for _, subtask := range oldIssue.Fields.Subtasks {
-		app.MigrateIssue(createdIssue.Key, subtask.Key)
+	// Attachments can't be set on create, they must be downloaded and posted later
+	for _, attachment := range issue.Fields.Attachments {
+		resp, err := http.Get(attachment.Content)
+		if err != nil {
+			dumpResponse(&jira.Response{Response: resp})
+			return nil, errors.Wrapf(err, "Error fetching attachment %q from issue %s", attachment.Filename, issue.Key)
+		}
+		defer resp.Body.Close()
+		if _, resp, err := app.To.Issue.PostAttachment(createdIssue.ID, resp.Body, attachment.Filename); err != nil {
+			dumpResponse(resp)
+			return nil, errors.Wrapf(err, "Error posting attachment %q to issue %s", attachment.Filename, createdIssue.Key)
+		}
 	}
 
-	return nil, nil
+	// Transition the ticket to the correct status
+	transitions, resp, err := app.To.Issue.GetTransitions(createdIssue.ID)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Error fetching transitions for %s", createdIssue.Key)
+	}
+	for _, transition := range transitions {
+		if transition.To.Name == issue.Fields.Status.Name {
+			if resp, err := app.To.Issue.DoTransition(createdIssue.ID, transition.ID); err != nil {
+				dumpResponse(resp)
+				return nil, errors.Wrapf(err, "Error transitioning issue %s to %q", createdIssue.Key, transition.To.Name)
+			}
+		}
+	}
+
+	// // Migrate all linked issues
+	// for _, link := range issue.Fields.IssueLinks {
+	// 	fmt.Printf("%+v\n", link)
+	// 	// only link outwardly to avoid dupes
+	// 	outward := link.OutwardIssue
+	// 	if outward != nil && outward.Key != issue.Key {
+	// 		outwardIssue, resp, err := app.Server.Issue.Get(outward.Key, &jira.GetQueryOptions{
+	// 			FieldsByKeys: true,
+	// 			Expand:       "names",
+	// 		})
+	// 		if err != nil {
+	// 			return nil, errors.Wrapf(err, "Error fetching outward link %s", outward.Key)
+	// 		}
+	// 		createdOutwardIssue, resp, err := app.MigrateIssue(nil, outwardIssue)
+	// 		if err != nil {
+	// 			return nil, errors.Wrapf(err, "Error migrating outward link %s", outward.Key)
+	// 		}
+	// 		app.To.Issue.AddLink(&jira.IssueLink{
+	// 			Type:         link.Type,
+	// 			OutwardIssue: createdOutwardIssue,
+	// 			InwardIssue:  createdIssue,
+	// 			// Comment:      link.Comment, // Too annoying to deal with right now (my god jira is complex)
+	// 		})
+	// 	}
+	// }
+
+	// Migrate all subtasks
+	// for _, subtask := range issue.Fields.Subtasks {
+	// 	subIssue, resp, err := app.From.Issue.Get(subtask.Key, &jira.GetQueryOptions{
+	// 		FieldsByKeys: true,
+	// 		Expand:       "names",
+	// 	})
+	// 	if err != nil {
+	// 		return nil, errors.Wrapf(err, "Error fetching subtask %s", subtask.Key)
+	// 	}
+	// 	if _, resp, err := app.MigrateIssue(createdIssue, subIssue); err != nil {
+	// 		return nil, errors.Wrapf(err, "Error migrating subtask %s", subIssue.Key)
+	// 	}
+	// }
+
+	// // Migrate all epic children
+	// if issue.Fields.Type.Name == "Epic" {
+	// 	issue.Fields.
+	// }
+
+	return createdIssue, nil
 }
 
-func getCloudUsers(cloud *jira.Client, projectKey string) ([]jira.User, *jira.Response, error) {
-	project, resp, err := cloud.Project.Get(projectKey)
+func getUsers(cloud *jira.Client, projectKey string) ([]jira.User, error) {
+	project, _, err := cloud.Project.Get(projectKey)
 	if err != nil {
-		return nil, resp, errors.Wrapf(err, "Error retrieving project %s", projectKey)
+		return nil, errors.Wrapf(err, "Error retrieving project %s", projectKey)
 	}
 
 	var users []jira.User
@@ -216,20 +319,19 @@ func getCloudUsers(cloud *jira.Client, projectKey string) ([]jira.User, *jira.Re
 	for role, resource := range project.Roles {
 		req, err := http.NewRequest("GET", resource, nil)
 		if err != nil {
-			return nil, nil, errors.Wrapf(err, "Error retrieving %s role from %s", role, resource)
+			return nil, errors.Wrapf(err, "Error retrieving %s role from %s", role, resource)
 		}
 		var role jira.Role
-		resp, err := cloud.Do(req, &role)
-		if err != nil {
-			return nil, resp, errors.Wrapf(err, "Error retrieving %s role from %s", role, resource)
+		if _, err := cloud.Do(req, &role); err != nil {
+			return nil, errors.Wrapf(err, "Error retrieving %s role from %s", role, resource)
 		}
 		for _, actor := range role.Actors {
-			user, resp, err := cloud.User.GetByAccountID(actor.ActorUser.AccountID)
+			user, _, err := cloud.User.GetByAccountID(actor.ActorUser.AccountID)
 			if err != nil {
-				return nil, resp, errors.Wrapf(err, "Error retrieving user %q", actor.DisplayName)
+				return nil, errors.Wrapf(err, "Error retrieving user %q", actor.DisplayName)
 			}
 			users = append(users, *user)
 		}
 	}
-	return users, nil, nil
+	return users, nil
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,15 @@ go 1.17
 
 require (
 	github.com/andygrunwald/go-jira v1.15.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/golang-jwt/jwt/v4 v4.3.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/trivago/tgo v1.0.7 // indirect
+	github.com/urfave/cli v1.22.5 // indirect
+	github.com/urfave/cli/v2 v2.3.0
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
